ea93fdace8191a6b7545c9a824f89e64c8873e92
jane semenova <semyonova.evg@yandex.ru>
2016-09-03 21:55:32 +0300

    Создана функция вычисления расстояния с функцией в общем классе
    Используется функция, а не метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,22 @@
 package ru.stqa.pft.sandbox;
 
+import static java.lang.Math.pow;
+
 public class MyFirstProgram {
+
   public static void main(String[] args) {
-    System.out.println("Hello, World");
+
+    Point p1 = new Point(25, 2);
+    Point p2 = new Point(3, 4);
+    System.out.print(distance(p1, p2));
+  }
+
+  public static double distance(Point p1, Point p2) {
+    double d;
+    d = Math.sqrt(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2));
+    return d;
   }
 }
+
+
 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Евгения on 03.09.2016.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+  this.x=x;
+  this.y=y;
+}
+
+}
