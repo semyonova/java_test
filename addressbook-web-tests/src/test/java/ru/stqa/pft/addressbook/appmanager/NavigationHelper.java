bcdaf07a4ceec67a11153defe8f94a17eee7d637
jane semenova <semyonova.evg@yandex.ru>
2016-10-08 19:15:54 +0300

    Проверка - не находимся ли мы уже на нужной странице

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -9,12 +9,27 @@ public class NavigationHelper extends BaseHelper {
     super(wd);
   }
 
-  public void gotoGroupPage() {click(By.linkText("groups"));
+  public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
+    click(By.linkText("groups"));
   }
 
-  public void gotoAddNewContact() {click(By.linkText("add new"));
+  public void gotoAddNewContact() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("Edit / add address book entry")) {
+      return;
+    }
+    click(By.linkText("add new"));
   }
 
-  public void goToHomePage() {click(By.linkText("home"));
+  public void goToHomePage() {
+    if (isElementPresent(By.id("maintable"))){
+      return;
+    }
+    click(By.linkText("home"));
   }
 }
