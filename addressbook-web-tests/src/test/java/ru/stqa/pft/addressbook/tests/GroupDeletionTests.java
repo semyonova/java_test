7d91ac8a9f7bbd9cf46a1020310931dece96ad9a
jane semenova <semyonova.evg@yandex.ru>
2016-10-08 21:16:16 +0300

    Revert "Revert "Добавлена проверка на предусловие для Групп""
    This reverts commit 07b05aad2377ff0c73882c4921448f0556ccceaf.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends BaseTest {
 
     @Test
     public void TestsGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereGroup()) {
+            app.getGroupHelper().CreateGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteGroup();
         app.getGroupHelper().returnToGroupPage();
