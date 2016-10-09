5d2850d4d7600ad5c8803cacb6268f30f7710e17
jane semenova <semyonova.evg@yandex.ru>
2016-10-09 12:23:51 +0300

    Добавлены проверки размера коллекции групп в тестах Модификации и Удаления групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -4,20 +4,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupDeletionTests extends BaseTest {
 
     @Test
     public void TestsGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
-        int beforeGroup = app.getGroupHelper().getGroupCount();
+
         if (! app.getGroupHelper().isThereGroup()) {
             app.getGroupHelper().CreateGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup(beforeGroup - 1);
+
+        List<GroupData> beforeGroup = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(beforeGroup.size() - 1);
         app.getGroupHelper().deleteGroup();
         app.getGroupHelper().returnToGroupPage();
-        int afterGroup = app.getGroupHelper().getGroupCount();
-        //Проверяем количство групп до и после выполнения теста
-        Assert.assertEquals(beforeGroup - 1, afterGroup);
+        List<GroupData> afterGroup = app.getGroupHelper().getGroupList();
+
+        //Проверяем количeство групп до и после выполнения теста
+        Assert.assertEquals(beforeGroup.size() - 1, afterGroup.size());
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,23 +4,28 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.List;
+
 public class GroupModificationTests extends BaseTest {
 
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
-    int beforeGroup = app.getGroupHelper().getGroupCount();
+
     if (! app.getGroupHelper().isThereGroup()) {
       app.getGroupHelper().CreateGroup(new GroupData("test1", null, null));
     }
-    app.getGroupHelper().selectGroup(beforeGroup - 1);
+
+    List<GroupData> beforeGroup = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(beforeGroup.size() - 1);
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("1", "test1", "test2"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
-    int afterGroup = app.getGroupHelper().getGroupCount();
+    List<GroupData> afterGroup = app.getGroupHelper().getGroupList();
+
     //Проверяем количeство групп до и после выполнения теста
-    Assert.assertEquals(beforeGroup, afterGroup);
+    Assert.assertEquals(beforeGroup.size(), afterGroup.size());
   }
 
 }
