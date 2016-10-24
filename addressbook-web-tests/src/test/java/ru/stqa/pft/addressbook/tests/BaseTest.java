5ac99020c6d07861f8d4c67cfa9b7bf9c2133681
jane semenova <semyonova.evg@yandex.ru>
2016-10-24 21:24:15 +0300

    Единый запуск браузера при запуске нескольких тестов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/BaseTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/BaseTest.java
@@ -1,21 +1,21 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class BaseTest {
 
 
-  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
-  @BeforeMethod
+  @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterMethod
+  @AfterSuite
   public void tearDown() {
     app.stop();
   }
