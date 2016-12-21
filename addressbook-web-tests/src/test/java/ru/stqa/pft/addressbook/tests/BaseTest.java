22411001c20213740abac4e82b5cebb97cd652e3
jane semenova <semyonova.evg@yandex.ru>
2016-12-21 22:22:19 +0300

    Создан конфигурационный файл
    Конфигурационный файл описывает свойства окружения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/BaseTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/BaseTest.java
@@ -8,7 +8,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 public class BaseTest {
 
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
 
   @BeforeSuite
   public void setUp() throws Exception {
